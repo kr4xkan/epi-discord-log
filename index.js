4d48b80c149af6a01a86850cda9a412b2baa911b
kr4xkan <kr4xkan@gmail.com>
2022-04-18 16:32:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const config = require("./config.json");
+
+let dumpChannel = null;
+
+client.on("ready", async () => {
+	dumpChannel = await client.channels.fetch(config.dumpChannel);
+	console.log(`listening for deleted messages in ${config.listenTo}`);
+});
+
+client.on("messageDelete", (message) => {
+	if (dumpChannel === null) return;
+	if (message.channel.type === "dm") return;
+	if (message.author.bot) return;
+	if (config.guildId !== message.guildId) return;
+	
+	let content = message.content;
+	let author = message.author.username;
+	let channel = message.channel.name;
+
+	const embed = new Discord.MessageEmbed()
+		.setColor('#0099ff')
+		.setTitle(`Message from ${author} in #${channel} deleted`)
+		.setDescription(content)
+		.setTimestamp()
+		.setFooter({ text: 'EpiLog' });
+
+	dumpChannel.send({ embeds: [embed] });
+});
+
+client.login(config.token);
